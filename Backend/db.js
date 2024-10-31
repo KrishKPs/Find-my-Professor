e65c1ba2af0c8443870b0cf3255f12e1d1bd8588
KrishKPs <kpskrishpatel@gmail.com>
2024-10-30 22:28:24 -0700

    Updated Schema

--- Backend/db.js
+++ Backend/db.js
@@ -8,5 +8,69 @@ mongoose.connect(process.env.MONGO_URL)
  
 
 
+const ProfessorSchema = mongoose.Schema({
+
+    name : {
+        type: String,
+        required: true, 
+    }, 
+    email : {
+        type: String,
+        required: true, 
+    }, 
+    password : {
+        type: String,
+        required: true, 
+    } 
+})
+
+
+
+const ProfessorDetailSchema = mongoose.Schema ({
+
+    name : {
+
+        type: String,
+        ref : 'Professor',    
+        required: true,  
+    }, 
+
+    email : {
+        type: String,
+        ref : 'Professor',  
+        required: true,     
+    }, 
+
+    profile_photo : {
+        type: String,
+        required: true,         
+    },
+    major : {
+        type: String,
+        required: true,         
+    },  
+    location : {
+        type: String,
+        required: true,         
+    }, 
+    office_hours : {
+        type: String,
+        required: true,         
+    },   
+    available : {
+        type: String,
+        required: true, 
+        enum : ['Available', 'Not-Available'],            
+    },   
+    verification : {
+        type: String,
+        required: true, 
+        enum : ['Verified', 'Not-Verified'],                
+
+    }
+
+
+
+})
 
 module.exports = mongoose;   
