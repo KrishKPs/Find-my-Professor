fd0fdd161d64178446d475210e19f32022bebd6b
KrishKPs <kpskrishpatel@gmail.com>
2024-11-03 22:34:39 -0800

    No OverWrite App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProfessorSignup from './Pages/SignupProfessor'
-import LoginUI from './Pages/LoginProfessor'
+
 import AdminSignup from './Pages/SignupAdmin'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     {/* <ProfessorSignup/> */}
-     {/* <LoginUI/> */}
      <AdminSignup/> 
     </>
   )
