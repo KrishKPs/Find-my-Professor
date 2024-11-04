fb0ffb3ad52b1656929dd2c7dcce591a3e597cdb
siddharth-09 <1234siddharth.panchal@gmail.com>
2024-11-04 11:36:41 +0530

    Added Login Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProfessorSignup from './Pages/SignupProfessor'
+import LoginUI from './Pages/LoginProfessor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <ProfessorSignup/>
+     {/* <ProfessorSignup/> */}
+     <LoginUI/>
     </>
   )
 }
