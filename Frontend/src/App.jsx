b3244fd30358e50cbe12c9c4c97870abe6f01217
Yug Patel <yugr2005@gmail.com>
2024-11-04 01:43:55 -0500

    added Router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 
 import AdminSignup from './Pages/SignupAdmin'
+import ProfessorSignup from './Pages/SignupProfessor'
+import LoginUI from './Pages/LoginProfessor'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { Route } from 'lucide-react'
 
 function App() {
 
@@ -8,6 +12,12 @@ function App() {
   return (
     <>
      <AdminSignup/> 
+     <BrowserRouter>
+     <Routes>
+      <Route path='/login' element={<LoginUI/>}/>
+      <Route path='/signup' element={<ProfessorSignup/>}/>
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
