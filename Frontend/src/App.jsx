a7e539d38b8eefef147169267a2fa6717ba2235e
siddharth-09 <1234siddharth.panchal@gmail.com>
2024-11-09 11:51:15 +0530

    Added Professor Detail

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import ProfessorSignup from './Pages/SignupProfessor'
 import LoginUI from './Pages/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AcademicFinder from './Pages/Finder'
+import Homepage from './Pages/Homepage'
+import ProfessorDetail from './Pages/ProfessorDetail'
 
 function App() {
 
@@ -14,7 +16,9 @@ function App() {
      <Routes>
       <Route path='/login' element={<LoginUI/>}/>
       <Route path='/signup' element={<ProfessorSignup/>}/>
-      <Route path='dashboard' element ={<AcademicFinder/>}/>  
+      <Route path='dashboard' element ={<AcademicFinder/>}/>
+      <Route path='homepage' element ={<Homepage/>}/>
+      <Route path='/' element ={<ProfessorDetail/>}/>   
      </Routes>
      </BrowserRouter>
     </>
